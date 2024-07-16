b07c20b3963341220cc8d97ac7ae3dd93c026354
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-16 18:15:50 +0500

    add the values

--- date_2015/script.js
+++ date_2015/script.js
@@ -3,4 +3,7 @@ function minutesCounter(datestring) {
     var datetoday = new Date();
     var differencetime = datetoday.getTime() - datebegin.getTime();
     var differenceminutes = (differencetime / 1000) / 60;
+    return differenceminutes;
 }
+
+var datestring = "2015-07-15";
