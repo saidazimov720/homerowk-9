01eb6d26f3b13ffafaf7a9c59c41ee4f67df0072
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-16 17:38:21 +0500

    the main logic

--- date_2015/script.js
+++ date_2015/script.js
@@ -1,4 +1,6 @@
 function minutesCounter(datestring) {
     var datebegin = new Date(datestring);
     var datetoday = new Date();
+    var differencetime = datetoday.getTime() - datebegin.getTime();
+    var differenceminutes = (differencetime / 1000) / 60;
 }
