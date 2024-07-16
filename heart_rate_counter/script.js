babd70d0b9890018fa8b4721ae4c21047ebe993c
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-16 21:36:14 +0500

    if else into if else

--- heart_rate_counter/script.js
+++ heart_rate_counter/script.js
@@ -4,7 +4,13 @@ var inputbpm = prompt("Enter the bpm:");
 if (inputdate && inputbpm) {
     var datestart = new Date(inputdate);
     var bpm = parseFloat(inputbpm);
-    
+
+    if (!isNaN(datestart.getTime()) && !isNaN(bpm) && bpm > 0) {
+        
+    } else {
+        
+    }
+
 } else {
     
 }
